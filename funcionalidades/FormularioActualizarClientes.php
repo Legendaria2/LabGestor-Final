11225999f56e6cd0215740664b2b11c592172811
APRENDIZ <santyq2003@gmail.com>
2024-12-02 09:35:21 -0500

    se agregaron rutas de bootstrap

--- funcionalidades/FormularioActualizarClientes.php
+++ funcionalidades/FormularioActualizarClientes.php
@@ -9,7 +9,8 @@
   <!-- Favicon -->
   <link rel="shortcut icon" href="./img/svg/logo.svg" type="image/x-icon">
   <!-- Custom styles -->
-  <!-- <link rel="stylesheet" href="./css/style.min.css"> -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="./css/style.min.css">
   <link rel="stylesheet" href="./css/style.css">
 </head>
 
@@ -255,6 +256,7 @@
   <!-- Chart library -->
   <script src="./plugins/chart.min.js"></script>
   <!-- Icons library -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   <script src="plugins/feather.min.js"></script>
   <!-- Custom scripts -->
   <script src="js/script.js"></script>
--- funcionalidades/Programas/IngresarClientes.php
+++ funcionalidades/Programas/IngresarClientes.php
@@ -9,8 +9,8 @@
   <!-- Favicon -->
   <link rel="shortcut icon" href="./img/svg/logo.svg" type="image/x-icon">
   <!-- Custom styles -->
-  <link rel="stylesheet" href="./css/style.min.css">
-  <link rel="stylesheet" href="./css/style.css">
+  <link rel="stylesheet" href="../css/style.min.css">
+  <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
